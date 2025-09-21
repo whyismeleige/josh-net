f42aa5c9abb9ab3f4eb37906a651de4faabb073b
whyismeleige <piyushjain31456@gmail.com>
2025-09-21 23:04:38 +0530

    Changed Suspense Problem

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,46 +1,10 @@
-"use client";
+import AuthCallback from "@/components/features/auth/AuthCallback";
+import { Suspense } from "react";
 
-import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useAuth } from "@/contexts/AuthContext";
-
-export default function AuthCallback() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const { loginViaGoogle } = useAuth();
-
-  useEffect(() => {
-    const handleCallback = async () => {
-      const accessToken = searchParams.get("accessToken");
-      const refreshToken = searchParams.get("refreshToken");
-      const error = searchParams.get("error");
-
-      if (error) {
-        console.error("Authentication error:", error);
-        const errorMessage = "Authentication failed";
-
-        router.push(`/auth/login?error=${encodeURIComponent(errorMessage)}`);
-        return;
-      }
-      if (accessToken && refreshToken) {
-        loginViaGoogle(accessToken, refreshToken);
-      } else {
-        router.push(
-          "/auth/login?error=" +
-            encodeURIComponent("No Authentication Required")
-        );
-      }
-    };
-    handleCallback();
-  }, [searchParams, router, loginViaGoogle]);
-
+export default function Page() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="text-center">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600 mx-auto mb-4"></div>
-        <p className="text-lg font-medium">Processing authentication...</p>
-        <p className="text-gray-600">Please wait while we log you in.</p>
-      </div>
-    </div>
+    <Suspense>
+      <AuthCallback />
+    </Suspense>
   );
 }
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -55,7 +55,7 @@ export default function LoginPage() {
         </div>
       </div>
       <div className="bg-muted relative hidden lg:block">
-        <Image
+        <img
           src="/placeholder.svg"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
